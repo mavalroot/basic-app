ba61e8705d3211d4c546cf8c2835a94467996690
María Valderrama <practicas@mavalroot.es>
2018-05-17 10:37:29 +0200

    Borrar

--- views/usuarios/delete.php
+++ views/usuarios/delete.php
@@ -1,12 +1,12 @@
 <?php
 include_once '../../config/main-local.php';
 
-use controllers\AparatosController;
+use controllers\UsuariosController;
 
 if (!isset($_POST['object_id'])) {
     echo 'El registro no ha podido ser eliminado.';
     return;
 } else {
-    $controller = new AparatosController();
+    $controller = new UsuariosController();
     $controller->delete($_POST['object_id']);
 }
